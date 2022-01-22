e0de150fa0e46f4ae3af67e58a6685b6e3e88810
Joseph Goldstone <jgoldstone@arri.com>
2022-01-22 16:28:29 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+# Write your program here
+brick.sound.beep()
+
+test_motor = Motor(Port.B)
+test_motor.run_target(500,90)
+brick.sound.beep(1000,500)
